1bba75911fbf98e20586c1d6f5a793d45d0fc57f
Arbaj Shaikh <sarbaj637@gmail.com>
2025-12-29 18:46:03 +0530

    ready for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use("/api/ai", router);
 app.get("/", (req, res) => {
   res.send("server is running ");
 });
-
-app.listen(8000, () => {
-  console.log(`server is running on http://localhost:8000`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
 });
